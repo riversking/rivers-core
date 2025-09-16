3a8688bafa9bfa631b892addce42ddaadc8dcff9
riversking <kingjames6236@163.com>
2025-09-17 00:20:24 +0800

    Enhance JWT security and update dependencies

--- src/main/java/com/rivers/core/util/JwtUtil.java
+++ src/main/java/com/rivers/core/util/JwtUtil.java
@@ -2,24 +2,41 @@ package com.rivers.core.util;
 
 import com.google.common.collect.Maps;
 import com.rivers.core.entity.LoginUser;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 
-import java.security.KeyPair;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.*;
 import java.util.Map;
+import java.util.UUID;
 
 @Slf4j
 public class JwtUtil {
 
-    private JwtUtil() {
-        throw new IllegalStateException("Utility class");
+
+    private static final SignatureAlgorithm signatureAlgorithm;
+
+    private static final KeyPair keyPair;
+
+
+    static {
+        signatureAlgorithm = Jwts.SIG.RS256;
+        try (InputStream is = JwtUtil.class.getClassLoader().getResourceAsStream("key/rivers.jks")) {
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(is, "King9128@918".toCharArray());
+            PrivateKey riversPrivate = (PrivateKey) keyStore.getKey("siyao", "King9128@918".toCharArray());
+            PublicKey riversPub = keyStore.getCertificate("siyao").getPublicKey();
+            keyPair = new KeyPair(riversPub, riversPrivate);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static String createJwt(LoginUser loginUser, String key) {
         // 获取私钥和证书
-        SignatureAlgorithm signatureAlgorithm = Jwts.SIG.RS256;
-        KeyPair keyPair = signatureAlgorithm.keyPair().build();
         Map<String, Object> chaim = Maps.newHashMap();
         chaim.put("loginUser", loginUser);
         // 构建JWT
@@ -32,5 +49,18 @@ public class JwtUtil {
                 .signWith(keyPair.getPrivate(), signatureAlgorithm)
                 .claims(chaim)
                 .compact();
+    }
+
+    public static Claims parseJwt(String token) {
+        PublicKey publicKey = keyPair.getPublic();
+        return Jwts.parser().verifyWith(publicKey).build().parseSignedClaims(token).getPayload();
+    }
+
+    public static void main(String[] args) {
+        LoginUser loginUser = new LoginUser();
+        loginUser.setAccountNo("a");
+        String jwt = createJwt(loginUser, UUID.randomUUID().toString());
+        System.out.println(jwt);
+        System.out.println(parseJwt(jwt));
     }
 }
