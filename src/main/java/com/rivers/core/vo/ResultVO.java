11ce26e0b994c21e1765b6e3c9db78a4002d4912
riversking <kingjames6236@163.com>
2025-09-26 00:14:11 +0800

    Enhance ResultVO with serialization and JSON annotations

--- src/main/java/com/rivers/core/vo/ResultVO.java
+++ src/main/java/com/rivers/core/vo/ResultVO.java
@@ -1,15 +1,29 @@
 package com.rivers.core.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class ResultVO<T> {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"code", "message", "data"})
+public class ResultVO<T extends Serializable> implements Serializable {
+
+
+    @Serial
+    private static final long serialVersionUID = 3278715392832902343L;
 
     private Integer code;
 
     private String message;
 
     private T data;
+
+    public ResultVO() {
+    }
 
     public ResultVO(T data) {
         this.data = data;
@@ -21,28 +35,35 @@ public class ResultVO<T> {
     }
 
     public ResultVO(Integer code, String message, T data) {
-        this.code = code;
-        this.message = message;
+        this(code, message);
         this.data = data;
     }
 
-    public static <T> ResultVO<T> ok(Integer code, String message) {
-        return new ResultVO<>(code, message);
-    }
-
-    public static <T> ResultVO<T> ok() {
+    public static ResultVO<EmptyType> ok() {
         return new ResultVO<>(200, "操作成功");
     }
 
-    public static <T> ResultVO<T> ok(T data) {
+
+    public static <T extends Serializable> ResultVO<T> ok(Integer code, String message) {
+        return new ResultVO<>(code, message);
+    }
+
+
+
+    public static <T extends Serializable> ResultVO<T> ok(T data) {
         return new ResultVO<>(200, "操作成功", data);
     }
 
-    public static <T> ResultVO<T> ok(Integer code, String message, T data) {
+    public static <T extends Serializable> ResultVO<T> ok(Integer code, String message, T data) {
         return new ResultVO<>(code, message, data);
     }
 
-    public static <T> ResultVO<T> fail(Integer code, String message) {
+    public static <T extends Serializable> ResultVO<T> fail(Integer code, String message) {
         return new ResultVO<>(code, message);
+    }
+
+    public abstract static class EmptyType implements Serializable {
+        @Serial
+        private static final long serialVersionUID = 1L;
     }
 }
